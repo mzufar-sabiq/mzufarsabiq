037917d2d75bc565976078f8ff85365d931be941
mzufar-sabiq <zufarsabiq1@gmail.com>
2023-02-04 09:24:30 +0700

    gbt

---
+++ index2.php
@@ -0,0 +1,14 @@
+<?php
+$angka = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+?>
+
+<?php foreach( $angka as $a ) : ?>
+<?php foreach ( $a as $b ) : ?>
+    <div class="kotak"><?= $a; ?></div>
+    <?php endforeach; ?> 
+    <div class="clear"></div> 
+<?php endforeach; ?> 
